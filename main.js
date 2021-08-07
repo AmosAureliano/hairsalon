a2c093f274946d6f8d769c2b077ce21c6d045df0
Amós Aureliano <amos.aureliano@gmail.com>
2021-08-07 18:20:45 -0300

    Implementar menu

---
+++ main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
+
+const links = document.querySelectorAll("#header li a");
+
+for(const element of toggle){
+    element.addEventListener('click', function(){
+        nav.classList.toggle('show');
+    })
+}
+
+
+for(const link of links){
+    link.addEventListener('click', function(){
+        nav.classList.remove('show');
+    })
+}
