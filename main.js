4ff0ac98bfd07444fd71f52c80e523ba3b53598e
Amós Aureliano <amos.aureliano@gmail.com>
2021-08-27 11:11:07 -0300

    Finalizar Versao Mobile

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const toggle = document.querySelectorAll("nav .toggle");
 
 const links = document.querySelectorAll("#header li a");
 
+
 for(const element of toggle){
     element.addEventListener('click', function(){
         nav.classList.toggle('show');
@@ -25,4 +26,28 @@ const swiper = new Swiper('.swiper-container', {
     mousewheel: true,
     keyboard: true
 
+})
+
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(`#home .text, #home .image,
+    #about .image, #about .text, #services heaader, #services .card,
+    #testimonials header, #testimonials .testimonials
+`, { interval: 100})
+
+
+//Back to top
+const backToTopButton = document.querySelector('.back-to-top');
+window.addEventListener('scroll', function(){
+    if(window.scrollY >= 560){
+        backToTopButton.classList.add('show')
+    }else{
+        backToTopButton.classList.remove('show')
+    }
 })
